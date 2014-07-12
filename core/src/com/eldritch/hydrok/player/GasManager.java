6588e6b372f8b6216890fe744fc0a964ebdda6b1
Travis Addair <tgaddair@gmail.com>
2014-07-11 22:34:18 -0700

    Added linear damping to the gas form

--- core/src/com/eldritch/hydrok/player/GasManager.java
+++ core/src/com/eldritch/hydrok/player/GasManager.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.eldritch.hydrok.GameScreen;
@@ -17,7 +18,7 @@ public class GasManager extends AbstractPhaseManager {
 	private float stateTime = 0;
 	
 	public GasManager(World world, int x, int y) {
-	    super(world, x, y, 0.25f, 0.3f, Settings.BIT_GAS);
+	    super(world, x, y, 0.20f, 0.3f, Settings.BIT_GAS);
 	    
 		TextureRegion[][] regions = GameScreen.getRegions("sprite/gas.png", 64, 64);
 		Array<TextureRegion> allRegions = new Array<TextureRegion>();
@@ -26,6 +27,9 @@ public class GasManager extends AbstractPhaseManager {
 		}
         animation = new Animation(0.15f, allRegions);
         animation.setPlayMode(Animation.PlayMode.LOOP);
+        
+        // air resistance to slow down the body
+        getBody().setLinearDamping(0.5f);
 	}
 	
 	@Override
@@ -33,9 +37,11 @@ public class GasManager extends AbstractPhaseManager {
 		stateTime += delta;
 		
 		// apply upwards impulse, but only if max velocity is not reached yet
-		Vector2 pos = getBody().getPosition();
-		if (getBody().getLinearVelocity().y < MAX_VELOCITY) {
-			getBody().applyLinearImpulse(0, 0.10f, pos.x, pos.y, true);
+		Body body = getBody();
+		Vector2 pos = body.getPosition();
+		if (body.getLinearVelocity().y < MAX_VELOCITY) {
+		    float force = Math.max(1 / (stateTime * 100), 0.01f);
+			body.applyLinearImpulse(0, force, pos.x, pos.y, true);
 		}
 	}
 	
